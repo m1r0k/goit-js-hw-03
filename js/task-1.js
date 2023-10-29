cf1604e7226d98e0e237354b7b21bbf01b8d3b6e
m1r0k <mir_ok@ukr.net>
2023-10-29 20:33:49 +0200

    29.10

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,10 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid;
-    if (totalPrice > customerCredits) {
-        return `Insufficient funds!`;
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+function slugify(title) {
+    let lowerTitle=title.toLowerCase();
+    let newList = lowerTitle.split(" ");
+    let newTitle = newList.join("-");
+    return newTitle;
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
