f3a430da7aa450514b2b4251f2fd5b362e0469a8
m1r0k <mir_ok@ukr.net>
2023-10-29 20:38:41 +0200

    29.10

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 function filterArray(numbers, value) {
     let newArray = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            newArray.push(numbers[i]);
+    for (const element of numbers){
+        if (element > value) {
+            newArray.push(element);
         }
     }
     return newArray;
